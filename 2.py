f2f74a43ec53aae0e7a2eda6f8c59e07b30f3c4e
SachikoNitta <32939720+SachikoNitta@users.noreply.github.com>
2025-05-07 09:26:52 +0000

    added problem 2

---
+++ 2.py
@@ -0,0 +1,15 @@
+nums = [2]
+left = 1
+right = 2
+
+while right < 4000000:
+    total = left + right
+    
+    if total % 2 == 0:
+        nums.append(total)
+    
+    left = right
+    right = total
+
+total_all = sum(nums)
+print(total_all)
