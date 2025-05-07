25ae17f834875f4c8e0b28d239c9fed72a6f5c75
SachikoNitta <32939720+SachikoNitta@users.noreply.github.com>
2025-05-07 13:16:02 +0000

    added problem 1 ver 2

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+# ver.1
 multiples = []
 for i in range(1000):
     if i % 3 == 0 or i % 5 == 0:
@@ -5,3 +6,9 @@ for i in range(1000):
 
 total = sum(multiples)
 print(total)
+
+# =======
+# ver.2
+# 答え = 3の倍数の和 + 5の倍数の和 - 15の倍数の和
+total = 3 * 333 * (1 + 333) / 2 + 5 * 200 * (1 + 200) / 2 - 15 * 66 * (1 + 66) / 2
+print(int(total))
