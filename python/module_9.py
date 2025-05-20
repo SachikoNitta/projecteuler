56c905a87d1737d38300a3e22b859b26d180f53c
SachikoNitta <32939720+SachikoNitta@users.noreply.github.com>
2025-05-20 06:02:25 +0000

    updated problem 9

--- python/module_9.py
+++ python/module_9.py
@@ -1,8 +1,20 @@
 def __main__():
-    for a in range(1, 333):
-        for b in range(a + 2, (998 - a) // 2):
+    # a + b + c = 1000 で　a < b < c なので、aは最大で332
+    for a in range(1, 332):
+        # a < bなのでbは最小でa + 1
+        # b < cなので、bは最大で(1000 - a - 1) // 2
+        for b in range(a + 1, (1000 - a - 1) // 2):
+            if a + b >= 1000:
+                break
+            
             # a + b + c = 1000
             c = 1000 - a - b
+
+            # a < b < c
+            if c <= b:
+                break
+
+            # a^2 + b^2 = c^2
             if a * a + b * b == c * c:
                 print(a, b, c)
                 print(a * b * c)
